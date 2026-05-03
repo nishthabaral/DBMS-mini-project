8ba650483b492a479d2b7750155181a170857efa
sample <baralnishtha7@gmail.com>
2026-05-04 00:42:29 +0530

    env setup

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Bengaluru67#$", 
-    database: "pharmacy_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
